34f8619a53b7cc920fcf599c76b81d08ffbf6def
js6g13 <js6g13@soton.ac.uk>
2017-02-05 18:28:09 +0100

    Added Base Tag

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <html lang="en">
 
 <head>
+    <base href="https://analysingnonsense.herokuapp.com/" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
