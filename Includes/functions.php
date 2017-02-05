3747f0ac9ed8a28d72336dfd6c93de9377a80ad2
js6g13 <js6g13@soton.ac.uk>
2017-02-05 18:05:04 +0100

    Finished most of the functionality

--- Includes/functions.php
+++ Includes/functions.php
@@ -2,29 +2,41 @@
     
     class Functions {
         
+        // Setting public variables for classwide call.
         public $stockResult;
         public $tickerResult;
         public $findSameWord;
         
+        /*
+        Function for retrieving all the values that will be used for comparing 
+        the twitter text to the existing list of entries in the DB.
+        */
         function getTable(){
             include "includes/db.php";
-            $getEntriesSentiment = $dbh->prepare("SELECT NAME FROM FullList");
+            $getEntriesSentiment = $dbh->prepare("SELECT * FROM FullList WHERE NAME LIKE '". $this->findSameWord ."%'");
             $getEntriesSentiment->execute();
-            $this->stockResult = $getEntriesSentiment -> fetchAll(PDO::FETCH_COLUMN, 0);
+            $this->stockResult = $getEntriesSentiment -> fetchAll(PDO::FETCH_COLUMN, 1); // PDO:FETCH_COLUMN, 1 is used in order to get the entire column with names.
             
         }
+        /*
+        This function is used in order to find the corresponding ticker Result of a company. E.g. Apple Inc -> AAPL.
+        */
         
         function getTicker(){
             if(!empty($this->findSameWord)){
                     include "includes/db.php";
-                    $getTicker = $dbh->prepare("SELECT TICKER  FROM FullList WHERE NAME LIKE '" .implode($this->findSameWord) ."%'");
+                    $getTicker = $dbh->prepare("SELECT * FROM FullList WHERE NAME LIKE '" .implode($this->findSameWord) ."'" );
                     $getTicker->execute();
-                    $this->tickerResult = $getTicker -> fetchAll(PDO::FETCH_COLUMN, 0);
-                    print_r($this->tickerResult);
+                    $this->tickerResult = $getTicker -> fetchAll(PDO::FETCH_COLUMN, 0); // PDO: FETCH_COLUMN is used in order to get the entire column with the ticker.                
                 }   
         }
         
+        /*
+        Main function that does sentiment analysis, twitter retrieval and yahoo API retrieval. 
+        */
         function twitterAnalysis(){
+            
+            
             require_once('TwitterAPIExchange.php');
             /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
             $settings = array(
@@ -45,7 +57,26 @@
                 $stringIntoArray = explode(" ", $items['text']);
                 $checkString = ($items['text']);
                 $checkArray = $this->stockResult;
+                
                 $this->findSameWord = array_intersect($stringIntoArray,$checkArray);
+                
+                //KEEP WORKING FROM HERE!!!
+                $result = $this->getTicker();
+                $tickerResult = $this->tickerResult['0']; 
+                $testa = "AAPL";
+                
+                
+                $tickerurl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22". $tickerResult . "%22)&format=json&env=http://datatables.org/alltables.env";
+                $tickercurl = curl_init();
+                curl_setopt ($tickercurl, CURLOPT_URL, $tickerurl);
+                curl_setopt($tickercurl, CURLOPT_RETURNTRANSFER, 1);
+                $tickerJson = curl_exec ($tickercurl);
+                json_decode($tickerJson);
+                curl_close($tickercurl);
+                $tickerdata = json_decode($tickerJson, true);
+                
+
+                
                 
                 $url = "http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment?apikey=895f984e23f58d468f2ade064d1b92725e0dc725&text=".urlencode($checkString)."&outputMode=json";
                 $curl = curl_init();
@@ -57,6 +88,45 @@
                 $data = json_decode($json, true);
                 
                 if (!empty($this->findSameWord) && isset($data['docSentiment']['score'])){
+                    if(($tickerdata['query']['results']['quote']['Bid']) !== NULL){
+                        echo '<pre>';
+                            echo "Symbol: ";
+                                echo '<b>';
+                                    echo ($tickerdata['query']['results']['quote']['symbol']);
+                                    echo ' ';
+                                echo '</b>';
+                            echo "Bid:";    
+                                echo '<b>';
+                                    echo($tickerdata['query']['results']['quote']['Bid']);
+                                    echo ' ';
+                                echo '</b>';
+
+                            echo "Change and Percent Change:";
+                                echo '<b>';
+                                    echo ($tickerdata['query']['results']['quote']['Change_PercentChange']);
+                                    echo ' ';
+                                echo '</b>';
+                        echo '</pre>';
+                    } else {
+                        echo '<pre>';
+                            echo "Symbol: ";
+                                echo '<b>';
+                                    echo "N/A";
+                                    echo ' ';
+                                echo '</b>';
+                            echo "Bid:";    
+                                echo '<b>';
+                                    echo"N/A";
+                                    echo ' ';
+                                echo '</b>';
+
+                            echo "Change and Percent Change:";
+                                echo '<b>';
+                                    echo "N/A";
+                                    echo ' ';
+                                echo '</b>';
+                        echo '</pre>';
+                    }
                     $result = $data['docSentiment']['score'];
                     echo "<center><b>KEYWORD FOUND: </b></center>";
                     echo "<i><center>". $items['text']."<br/>". $result. "<br /><hr> </center></i>";
@@ -68,12 +138,5 @@
             }
         }
     }
-    
-    
-
-
-    
-    
-    
     
 ?>
